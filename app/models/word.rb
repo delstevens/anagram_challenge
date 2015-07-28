b380c8cd178e9f96a9c7734e38c12ba361b85f79
Del Stevens <del.stevens@gmail.com>
2015-07-28 20:08:45 +1200

    elegant and fast (without 'tweaking' the database)

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,10 +1,16 @@
 class Word < ActiveRecord::Base
 
   def self.anagram(in_word)
-      this_array = []
-      this_array << 1
-      this_array << Word.first.word_name + " #{in_word}"
-      this_array << 2
+  		ordered_word = in_word.split("").sort.join("")
+  		return_array = []
+  		this_array =  Word.where("length(word_name) = #{in_word.length}")
+  		this_array.each do |item| 
+  			if ordered_word == item.word_name.split("").sort.join("")
+	  			return_array << item.word_name
+	  		end
+	  	end
+	  	return_array
+
   end
 
 
